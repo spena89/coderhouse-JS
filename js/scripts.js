8ad963548cc1b0dcd0f6f71e1a3915979507adea
santiago Pena <psanti89@gmail.com>
2022-07-11 23:47:16 -0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,33 @@
-// const valorIngresado = prompt("ingresa la cantidad de veces que quieres
-// repetir el saludo") for (let i=0; i<valorIngresado; i++){ console.log("hola")
-// } let entradaUsuario = "" let salida = "" while (entradaUsuario != "esc") {
-// entradaUsuario = prompt("ingreses su palabra")     salida += entradaUsuario +
-// " "     if (entradaUsuario != "esc") {         console.log(salida)     } }
-
 function calcularCuota() {
     let salir = false;
     let monto;
     while (salir === false) {
         monto = prompt(
-            "ingrese el valor total del producto, para salir presione la tecla esc"
+            "Ingrese el valor total del producto, para salir presione la tecla esc"
         )
         if (monto != null) {
             if (!isNaN(monto) && monto > 0) {
-                let cuotas = parseInt(prompt("en cuantas cuotas lo quiere pagar?"))
+                let cuotas = parseInt(prompt("En cuantas cuotas lo quiere pagar?"))
                 let totalPorCuota;
-                if (cuotas <= 12 && cuotas > 0) {
+                if (!isNaN(cuotas) && cuotas <= 12 && cuotas > 0) {
                     totalPorCuota = monto / cuotas
                     salir = true;
                     alert(
-                        "ustede debe pagar " + cuotas + " cuotas de " + totalPorCuota + " pesos"
+                        "Usted debe pagar " + cuotas + " cuotas de " + totalPorCuota + " pesos"
                     )
                     return totalPorCuota;
                 } else {
-                    alert("debe ingresar una cantidad de cuotas menor a 12")
+                    alert("Debe ingresar una cantidad de cuotas menor a 12")
                 }
             } else {
-                alert("debe ingresar un número mayor a 0")
+                alert("Debe ingresar un número mayor a 0")
             }
         } else {
             alert("Gracias por usar la calculadora de cuotas")
             salir = true;
         }
     }
+    return null;
 }
 let valorCuota = calcularCuota();
-//scope - importante entenderlo
+//prueba de sync github
