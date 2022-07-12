daf1511daf45c04ba75d3262511c72a320ca4ec3
santiago Pena <psanti89@gmail.com>
2022-07-11 23:13:31 -0300

    funcion calcularCuota ok

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,34 @@
 // " "     if (entradaUsuario != "esc") {         console.log(salida)     } }
 
 function calcularCuota() {
-    let i = 1;
-    let monto = "esc"
-    while (i === 1) {
+    let salir = false;
+    let monto;
+    while (salir === false) {
         monto = prompt(
             "ingrese el valor total del producto, para salir presione la tecla esc"
         )
-        if (monto != "esc") {
-            console.log(monto)
-            let cuotas = parseInt(prompt("en cuantas cuotas lo quiere pagar?"))
-            let totalPorCuota;
-            if (cuotas <= 12) {
-                totalPorCuota = monto / cuotas
-                i = 0;
-                alert(
-                    "ustede debe pagar " + cuotas + " cuotas de " + totalPorCuota + " pesos"
-                )
-                return totalPorCuota;
+        if (monto != null) {
+            if (!isNaN(monto) && monto > 0) {
+                let cuotas = parseInt(prompt("en cuantas cuotas lo quiere pagar?"))
+                let totalPorCuota;
+                if (cuotas <= 12 && cuotas > 0) {
+                    totalPorCuota = monto / cuotas
+                    salir = true;
+                    alert(
+                        "ustede debe pagar " + cuotas + " cuotas de " + totalPorCuota + " pesos"
+                    )
+                    return totalPorCuota;
+                } else {
+                    alert("debe ingresar una cantidad de cuotas menor a 12")
+                }
             } else {
-                alert("debe ingresar una cantidad de cuotas menor a 12")
+                alert("debe ingresar un número mayor a 0")
             }
         } else {
             alert("Gracias por usar la calculadora de cuotas")
-            i=0
+            salir = true;
         }
     }
-
 }
 let valorCuota = calcularCuota();
 //scope - importante entenderlo
