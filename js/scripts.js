435855f435f54a23a034fcc30fc8d0c66d24f8c9
santiago Pena <psanti89@gmail.com>
2022-08-15 02:21:28 -0300

    modal and cart functionalities added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let cart = JSON.parse(localStorage.getItem("cart")) ?? [];
-
+payButtonSuccess();
 /// CLASSES /
 class Product {
     constructor(name, img, price, description, id, color, stock) {
@@ -22,18 +22,20 @@ class CartItem {
 
 /// CART FUNCTIONS /
 function addToCart(product, qty) {
-    // falta implementar si item existe en carrito
     if (product.stock >= qty) {
         let index = cart.findIndex(object => {
             return object.product.id === product.id;
         });
-        //if ternario. Si item existe en carrito --> aumentar qty. Sino agregar al carrito
+        // if ternario. Si item existe en carrito --> aumentar qty. Sino agregar al carrito
         index != -1 ? cart[index].qty += qty : cart.push(new CartItem(product, qty));
-
         product.stock -= qty;
         localStorage.setItem("cart", JSON.stringify(cart));
     } else {
-        alert("no queda más stock")
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Looks like we are out of stock"}
+            )
     }
     updateCartInfo();
     updateContainer();
@@ -53,39 +55,34 @@ function deleteItemFromCart() {
         updateCartInfo();
         updateContainer();
     } else {
-        alert("El producto no se encuentra en el carrito");
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "The product is not in the cart anymore"}
+            );
     }
 }
 
 function emptyCart() {
     cart = [];
-    document.getElementById("cartTotal").innerHTML = "empty";
+    document.getElementById("cartTotal").innerHTML = "";
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 /// HTML FUNCTIONS /
 
+/// generates cart cards in modal of the cart preview
 function generateCartElements() {
     let modal = document.getElementById("cardsModal")
     modal.innerHTML = ``
     cart.forEach((cartItem) => {
         let item = document.createElement("tr")
         item.setAttribute("id",cartItem.product.id.toString())
-        item.innerHTML = `
-            <th scope = "row">-  ${cartItem
-            .product
-            .id} </th>
-            <td>-  ${cartItem
-            .product
-            .name} </td>
-            <td>- <img style="width: 50px" src="${cartItem
-            .product
-            .img}"> </td>
-            <td> - ${cartItem
-            .product
-            .price} </td>
-            <td> - ${cartItem
-            .qty} </td>
+        item.innerHTML = `            
+            <td>${cartItem.product.name} </td>
+            <td> <img style="width: 50px" src="${cartItem.product.img}"> </td>
+            <td>${cartItem.product.price} </td>
+            <td>${cartItem.qty} </td>
             <td><button class ="btn-danger"> delete </button></td>
             `
             item.getElementsByClassName("btn-danger")[0].addEventListener('click', deleteItemFromCart, false)
@@ -93,43 +90,59 @@ function generateCartElements() {
     })
 }
 
+
 function populateContainer() {
     const contenedorProductos = document.getElementById("contenedorProductos");
     contenedorProductos.innerHTML = "";
     productsCatalog.forEach((product) => {
         const idBtn = `AddToCart${product.id}`;
         contenedorProductos.innerHTML += `
-    <div class="col-md-4 my-4">
-        <div class="card" style="width: 18rem;">
-            <img src=${product.img} class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.description}</p>
-                <p class="card-text color-succes">$${product.price}</p>
-                <p class="card-text">Quedan: ${product.stock} disponibles!</p>
-                <button class="btn btn-primary" id=${idBtn}>Agregar al Carrito</button>
+            <div class="col-md-4 my-4">
+                <div class="card" style="width: 18rem;">
+                    <img src=${product.img} class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.description}</p>
+                        <p class="card-text color-succes">$${product.price}</p>
+                        <p class="card-text">Quedan: ${product.stock} disponibles!</p>
+                        <button class="btn btn-primary" id=${idBtn}>Agregar al Carrito</button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    `;
+        `;
     });
 }
 
 //Add to cart button functionality
+function payButtonSuccess(){
+    document.getElementById("payButton").addEventListener("click", () =>{
+        if(cart.length > 0){
+            Swal.fire({
+                icon: 'success',
+                title: 'Thank you',
+                text: 'Your order has been placed correctly!',
+                })
+                emptyCart()
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'There are no products on the cart',
+                // text: 'Your order has been placed correctly!',
+                })
+            }
+        });
+    }
+
 function prepareButtons() {
     productsCatalog.forEach((product) => {
         const idBtn = `AddToCart${product.id}`;
-        document
-            .getElementById(idBtn)
-            .addEventListener("click", () => {
+        document.getElementById(idBtn).addEventListener("click", () => {
                 addToCart(product, 1);
             });
     });
-    document
-        .getElementById("emptyCart")
-        .addEventListener("click", () => {
+    document.getElementById("emptyCart").addEventListener("click", () => {
             emptyCart();
-        });
+        });        
 }
 
 //updates stock visible by users on HTML
@@ -147,9 +160,12 @@ function updateCartInfo() {
     cart.forEach((cartItem) => {
         qty += cartItem.qty
     })
-    document
-        .getElementById("cartTotal")
-        .innerHTML = qty.toString() + "- $" + total;
+    if(qty > 0){
+        document.getElementById("cartTotal").innerHTML = qty.toString() + "- $" + total;
+    }else{
+        emptyCart();
+    }
+
 }
 
 /// PRODUCTS CATALOG  /
