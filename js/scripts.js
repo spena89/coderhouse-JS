585d4ce96d55e10f45421c4c265d489a7e149bd9
santiago Pena <psanti89@gmail.com>
2022-08-17 03:11:02 -0300

    fetch implemented. TODO:update container stock

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,4 @@
 let cart = JSON.parse(localStorage.getItem("cart")) ?? [];
-payButtonSuccess();
-/// CLASSES /
-class Product {
-    constructor(name, img, price, description, id, color, stock) {
-        this.name = name;
-        this.img = img;
-        this.price = price;
-        this.description = description;
-        this.id = id;
-        this.color = color;
-        this.stock = stock;
-    }
-}
 
 class CartItem {
     constructor(product, qty) {
@@ -79,10 +66,10 @@ function generateCartElements() {
         let item = document.createElement("tr")
         item.setAttribute("id",cartItem.product.id.toString())
         item.innerHTML = `            
-            <td>${cartItem.product.name} </td>
-            <td> <img style="width: 50px" src="${cartItem.product.img}"> </td>
-            <td>${cartItem.product.price} </td>
-            <td>${cartItem.qty} </td>
+            <td class = "product-table"><img class = "cart-img" src="${cartItem.product.img}">${cartItem.product.name}</td>
+            <td class = "product-table">${cartItem.product.price} </td>
+            <td class = "product-table">${cartItem.qty} </td>
+            <td class = "product-table">${parseInt(cartItem.qty * cartItem.product.price)} </td>
             <td><button class ="btn-danger"> delete </button></td>
             `
             item.getElementsByClassName("btn-danger")[0].addEventListener('click', deleteItemFromCart, false)
@@ -94,23 +81,27 @@ function generateCartElements() {
 function populateContainer() {
     const contenedorProductos = document.getElementById("contenedorProductos");
     contenedorProductos.innerHTML = "";
-    productsCatalog.forEach((product) => {
-        const idBtn = `AddToCart${product.id}`;
-        contenedorProductos.innerHTML += `
-            <div class="col-md-4 my-4">
-                <div class="card" style="width: 18rem;">
-                    <img src=${product.img} class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">${product.description}</p>
-                        <p class="card-text color-succes">$${product.price}</p>
-                        <p class="card-text">Quedan: ${product.stock} disponibles!</p>
-                        <button class="btn btn-primary" id=${idBtn}>Agregar al Carrito</button>
+    fetch("Products.json")
+    .then((response) => response.json())
+    .then(productsCatalog => {
+        productsCatalog.forEach((product) => {
+            const idBtn = `AddToCart${product.id}`;
+            contenedorProductos.innerHTML += `
+                <div class="col-md-4 my-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src=${product.img} class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                            <p class="card-text">${product.description}</p>
+                            <p class="card-text color-succes">$${product.price}</p>
+                            <p class="card-text">Quedan: ${product.stock} disponibles!</p>
+                            <button class="btn btn-primary" id=${idBtn}>Agregar al Carrito</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `;
-    });
+            `;
+        });
+    })
 }
 
 //Add to cart button functionality
@@ -134,16 +125,21 @@ function payButtonSuccess(){
     }
 
 function prepareButtons() {
-    productsCatalog.forEach((product) => {
-        const idBtn = `AddToCart${product.id}`;
-        document.getElementById(idBtn).addEventListener("click", () => {
+    fetch("Products.json")
+    .then((response) => response.json())
+    .then(productsCatalog => {
+        productsCatalog.forEach((product) => {
+            const idBtn = `AddToCart${product.id}`;
+            document.getElementById(idBtn).addEventListener("click", () => {
                 addToCart(product, 1);
             });
+        })
     });
     document.getElementById("emptyCart").addEventListener("click", () => {
             emptyCart();
         });        
-}
+    
+    }
 
 //updates stock visible by users on HTML
 function updateContainer() {
@@ -153,7 +149,7 @@ function updateContainer() {
 
 function updateCartInfo() {
     const total = cart.reduce(
-        (acumulator, cartItem) => acumulator + cartItem.product.price,
+        (acumulator, cartItem) => acumulator + cartItem.product.price * cartItem.qty,
         0
     );
     let qty = 0
@@ -168,41 +164,9 @@ function updateCartInfo() {
 
 }
 
-/// PRODUCTS CATALOG  /
-
-const product1 = new Product(
-    "Nvidia RTX 3080",
-    "https://ddtech.mx/assets/uploads/07b9dd6cc5f40598fabc9546c27712b5.png",
-    1500,
-    "High end graphics card",
-    1,
-    "black",
-    10
-);
-const product2 = new Product(
-    "Nvidia RTX 3070",
-    "https://as01.epimg.net/meristation/imagenes/2020/11/04/reportajes/1604481781_2" +
-            "48646_1604481912_noticia_normal.jpg",
-    1200,
-    "High performing mid-range graphics card",
-    2,
-    "black",
-    10
-);
-const product3 = new Product(
-    "Nvidia RTX 3060",
-    "https://thotcomputacion.com.uy/wp-content/uploads/2021/03/12G-P5-3657-KR_LG_16" +
-            "29_7b1e27934adb49418cd71fa64f868981.png",
-    1000,
-    "Best bang for the buck graphics card mid-tier",
-    3,
-    "black",
-    10
-);
-
-const productsCatalog = [product1, product2, product3];
 
 /// INITIALIZE SITE /
 
 updateContainer();
 updateCartInfo();
+payButtonSuccess();
